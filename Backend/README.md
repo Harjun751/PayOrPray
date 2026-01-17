5538434f01f017d1c7c91893274a0a4467651a30
Harjun751 <Harjun751@gmail.com>
2026-01-17 15:28:23 +0800

    Add DB connection

--- Backend/README.md
+++ Backend/README.md
@@ -1,2 +1,5 @@
 # Backend
 [API Spec](https://app.swaggerhub.com/apis/harjun7517/PayOrPray/0.1)
+
+# Environment Variables
+Setup a .env file in this folder. Follow .env.example
--- Backend/controller/app.js
+++ Backend/controller/app.js
@@ -1,4 +1,18 @@
+// Environment variables
+require('dotenv').config();
+
+// DB setup
+const pgp = require('pg-promise')();
+let conn = process.env.DATABASE_CONN_STRING;
+const db = pgp(conn);
+
+// Express
 var express = require('express');
 var app = express();
 
 module.exports = app;
+
+app.get("/test", async (req, res) => {
+    let dbres = await db.any('SELECT * FROM USERS');
+    res.send(dbres);
+})
